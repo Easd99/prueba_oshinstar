2ae705f5e22b9328367c01c4ee187f96c2104c31
Easd99 <solanoae@uninorte.edu.co>
2022-07-30 15:37:22 -0500

    fix: fixed errors

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,22 @@
+FROM node:14 AS build
+ARG node_prune="true"
+
+WORKDIR /usr/src/app
+COPY package*.json ./
+COPY . .
+RUN npm install
+RUN npm run tsc
+
+# remove development dependencies
+RUN npm prune --production
+
+## this is stage two , where the app actually runs
 FROM node:14
 
 WORKDIR /usr/src/app
-
 COPY package*.json ./
+COPY doc.yaml ./
+RUN npm install --only=production
+COPY --from=build /usr/src/app/dist ./dist
 
-RUN npm ci --only=production
-
-
-COPY . .
-
-CMD [ "npm", "start" ]
+CMD ["npm", "run", "start"]
--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,33 @@
 import "reflect-metadata";
-import express from "express"
+import express from "express";
 import indexRoutes from "./routes/index.routes";
 //import cors from  'cors'
-const cors = require('cors')
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./doc.yaml');
-import db from './models';
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./doc.yaml");
+import db from "./models";
 
-const app = express()
-if (process.env.NODE_ENV !== 'test') {
-    const connect = async () => {
-        try {
-            db.sequelize.sync().then(() => {})
-            await console.log('DB CONNECTED')
-        } catch (e) {
-            process.exit()
-        }
+const app = express();
+if (process.env.NODE_ENV !== "test") {
+  const connect = async () => {
+    try {
+      db.sequelize.sync().then(() => {
+        console.log("DB CONNECTED");
+      });
+    } catch (e) {
+      process.exit();
     }
-    const bd = connect()
+  };
+  const bd = connect();
 }
-
 
 //Middelwares
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
+app.set("port", process.env.PORT || 5000);
+app.use("/api/v1", indexRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-
-
-app.set('port', process.env.PORT || 5000)
-app.use('/api/v1', indexRoutes)
-//app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-export default app
+export default app;
